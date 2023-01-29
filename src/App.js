7fc88612db941fd41282f6bd59a8bb8824f7bf37
jejovanni <jamaineekong@gmail.com>
2023-01-29 01:25:20 +0100

    building blocks

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import Feed from './Feed';
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+
+            {/*Sidebar*/}
+            <Sidebar />
+            {/*Feed*/}
+            <Feed />
+            {/*Widgets*/}
+        </div>
+    );
+}
+
+export default App;
